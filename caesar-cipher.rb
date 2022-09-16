38100738578927e7f745d98fb8de5c495814c2b7
johannkrugell <johannkrugell@gmail.com>
2022-09-17 09:33:05 +1000

    CHARACTER ARRAY TO INTERGER ARRAY Add iteration to convert each character to an interger
    On branch string_to_integer
    Changes to be committed:
    modified:   caesar-cipher.rb

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,7 +1,16 @@
 #require ''
 
 def caeser_cipher(string, shift_factor)
- character_array = string.split(//)
+  # convert string to an array of characters
+  character_array = string.split(//)
+
+  integer_array = []
+
+  # loop through charater array convert to integers
+  character_array.each do |character|
+    integer_array.push << character.ord
+  end
+
 end
 
 p caeser_cipher("cat",0)
