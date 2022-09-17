490db6f2fdcc4a4ab7d9d7225c93dd539da1818d
johannkrugell <johannkrugell@gmail.com>
2022-09-18 05:29:19 +1000

    INTERGER SHIFT AND CONVERT TO CHARACTER Added the number of characters to shift and converted the array back to characters from integers
    On branch intergershift_to_character
    Changes to be committed:
    modified:   caesar-cipher.rb

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -8,9 +8,15 @@ def caeser_cipher(string, shift_factor)
 
   # loop through charater array convert to integers
   character_array.each do |character|
-    integer_array.push << character.ord
+    integer_array.push << character.ord+shift_factor
   end
 
+  cipher_array = []
+  # loop through integer array convert to character
+  integer_array.each do |integer| 
+    cipher_array.push << integer.chr
+  end
+  return cipher_array.join
 end
 
-p caeser_cipher("cat",0)
+p caeser_cipher("cat on mat",1)
