8c0f45206902ee4466f030a9855d8c59f3826104
johannkrugell <johannkrugell@gmail.com>
2022-09-20 05:27:03 +1000

    A TO Z WRAP I included a shift wrap helper that add or subtracts 26 from the ordinal number. If the ordinal value of the character is outside of the ASCII range for upper or lowercase letters then the shift wrap helper is used to wrap from a to z.
    On branch wrap-z-to-a
    Changes to be committed:
    modified:   caesar-cipher.rb

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,29 +1,52 @@
 require 'pry-byebug'
 
 def caeser_cipher(string, shift_factor)
-  # convert string to an array of characters
+  # declare array of upper and lowecase ascii numbers
+  uppercase_ascii_range = (65..90).to_a
+  lowercase_ascii_range = (97..122).to_a
+
+  # declare array to store integer and ciphered text
+  integer_array = []
+  cipher_array = []
+
+  # split string into character array
   character_array = string.split(//)
 
-  p character_array
-  integer_array = []
-
-  # loop through charater array convert to integers
+  # loop through charater array convert to integers, append to integer array
   character_array.each do |character|
-    if character.ord >= 65 && character.ord <= 90 ||
-      character.ord >= 97 && character.ord <= 122
-        integer_array.push << character.ord+shift_factor
-    else
-        integer_array.push << character
+    if character.match(/[A-Z]+/) && 
+       uppercase_ascii_range.include?(character.ord + shift_factor)
+      integer_array.push << character.ord + shift_factor
+    elsif character.match(/[a-z]+/) && 
+        lowercase_ascii_range.include?(character.ord + shift_factor)
+      integer_array.push << character.ord + shift_factor
+    elsif character.match(/[a-zA-Z]+/)
+      integer_array.push << character.ord + shift_factor + 
+                            shift_factor_wrap(shift_factor) 
+    else 
+      integer_array.push << character.ord 
     end
   end
-
-  cipher_array = []
   
   # loop through integer array convert to character
   integer_array.each do |integer| 
     cipher_array.push << integer.chr
   end
+  # return the cipher array as text
   return cipher_array.join
 end
 
-p caeser_cipher("Cat, on!",2)
+# helper function to wrap from z to a 
+def shift_factor_wrap(shift_factor) 
+  if shift_factor < 0 
+    return 26 
+  else
+    return -26
+  end
+end
+
+# Test cipher: "Attack at once!" with a left shift of 3
+p caeser_cipher("Attack at once!",-3)
+
+# Test decipher: "Attack at once!" with a right shift of 3
+p caeser_cipher("Xqqxzh xq lkzb!",3)
